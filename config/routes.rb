7e4ab92fb8a4d5056b383922105c9a5c274e1ffc
SatanPetitCoeur <plantevin.contact@gmail.com>
2018-09-03 17:40:26 +0200

    routes ok

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
 
-  devise_for :shops, path: 'admins', controllers: { sessions: "devise_shops/sessions", registrations: "devise_shops/registrations",
-  passwords: "devise_shops/passwords" }
+  devise_for :shops, path: 'shops', controllers: { sessions: "shops/sessions", registrations: "shops/registrations",
+  passwords: "shops/passwords" }
 
-  devise_for :relays, path: 'admins', controllers: { sessions: "devise_relays/sessions", registrations: "devise_relays/registrations",
-  passwords: "devise_relays/passwords" }
+  devise_for :relays, path: 'relays', controllers: { sessions: "relays/sessions", registrations: "relays/registrations",
+  passwords: "relays/passwords" }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
