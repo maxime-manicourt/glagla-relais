c11632ddd50d807f6ad59e084817cd4e6c11caa5
SatanPetitCoeur <plantevin.contact@gmail.com>
2018-09-03 15:54:53 +0200

    créa model replay

---
+++ db/migrate/20180903132816_add_attributes_to_relays.rb
@@ -0,0 +1,11 @@
+class AddAttributesToRelays < ActiveRecord::Migration[5.2]
+  def change
+    add_column :relays, :company_name, :string
+    add_column :relays, :contact, :string
+    add_column :relays, :address, :string
+    add_column :relays, :phone, :string
+    add_column :relays, :mail, :string
+    add_column :relays, :schedule, :string
+    add_column :relays, :capacity, :integer
+  end
+end
